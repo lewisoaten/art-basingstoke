fe7717fa6dac15f623b3dc74f7e898cdce65faf2
Lewis Oaten <lewis@oaten.name>
2018-06-11 21:09:18 +0100

    Updated art in listing

--- scripts/artList.js
+++ scripts/artList.js
@@ -1,9 +1,9 @@
 const data = [
   {
     id: 1,
-    title: "All Our Own Crafts",
+    title: "Annabel4 2 copy",
     description: "...",
-    imgAsset: "All_Our_Own_Crafts.webp",
+    imgAsset: "Annabel4 2 copy.jpg",
     mtlAsset: "3d-model.mtl",
     objAsset: "3d-model.obj",
     scale: "2 2 2",
@@ -11,9 +11,29 @@ const data = [
   },
   {
     id: 2,
-    title: "Annabel 1",
+    title: "Annabel4 2",
     description: "...",
-    imgAsset: "Annabel1.webp",
+    imgAsset: "Annabel4 2.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 3,
+    title: "BasingstokeArtClass copy",
+    description: "...",
+    imgAsset: "BasingstokeArtClass copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 4,
+    title: "Emily_Mcbrayne 2 copy",
+    description: "...",
+    imgAsset: "Emily_Mcbrayne 2 copy.jpg",
     mtlAsset: "3d-model.mtl",
     objAsset: "3d-model.obj",
     scale: "2 2 2",
@@ -21,9 +41,149 @@ const data = [
   },
   {
     id: 5,
-    title: "Annabel 4",
+    title: "Emily_Mcbrayne 3 copy",
     description: "...",
-    imgAsset: "1.jpg",
+    imgAsset: "Emily_Mcbrayne 3 copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 6,
+    title: "Emily_Mcbrayne copy",
+    description: "...",
+    imgAsset: "Emily_Mcbrayne copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 7,
+    title: "Helena_iPad copy",
+    description: "...",
+    imgAsset: "Helena_iPad copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 8,
+    title: "iPad_1 2 copy",
+    description: "...",
+    imgAsset: "iPad_1 2 copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 9,
+    title: "Lillian-_Let's_Make_A_Deal 2 copy",
+    description: "...",
+    imgAsset: "Lillian-_Let's_Make_A_Deal 2 copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 10,
+    title: "Lillian-_Let's_Make_A_Deal copy",
+    description: "...",
+    imgAsset: "Lillian-_Let's_Make_A_Deal copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 11,
+    title: "Lissie_ 2 copy",
+    description: "...",
+    imgAsset: "Lissie_ 2 copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 12,
+    title: "Lissie_ copy",
+    description: "...",
+    imgAsset: "Lissie_ copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 13,
+    title: "Maddie1 copy",
+    description: "...",
+    imgAsset: "Maddie1 copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 14,
+    title: "Scream_iPad copy",
+    description: "...",
+    imgAsset: "Scream_iPad copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 15,
+    title: "Student1a copy",
+    description: "...",
+    imgAsset: "Student1a copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 16,
+    title: "Student2a copy",
+    description: "...",
+    imgAsset: "Student2a copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 17,
+    title: "Studentbb copy",
+    description: "...",
+    imgAsset: "Studentbb copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 18,
+    title: "Studentcc_ copy",
+    description: "...",
+    imgAsset: "Studentcc_ copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 19,
+    title: "Studentdd_ copy",
+    description: "...",
+    imgAsset: "Studentdd_ copy.jpg",
     mtlAsset: "3d-model.mtl",
     objAsset: "3d-model.obj",
     scale: "2 2 2",
@@ -31,9 +191,49 @@ const data = [
   },
   {
     id: 20,
-    title: "Basingstoke Art Class",
+    title: "Students copy",
     description: "...",
-    imgAsset: "2.jpg",
+    imgAsset: "Students copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 21,
+    title: "ThatGallery copy",
+    description: "...",
+    imgAsset: "ThatGallery copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 22,
+    title: "Viable copy",
+    description: "...",
+    imgAsset: "Viable copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 23,
+    title: "When_Robots_Attack_-_Proteus copy",
+    description: "...",
+    imgAsset: "When_Robots_Attack_-_Proteus copy.jpg",
+    mtlAsset: "3d-model.mtl",
+    objAsset: "3d-model.obj",
+    scale: "2 2 2",
+    isObj: true
+  },
+  {
+    id: 24,
+    title: "Willis_Museum",
+    description: "...",
+    imgAsset: "Willis_Museum.jpg",
     mtlAsset: "3d-model.mtl",
     objAsset: "3d-model.obj",
     scale: "2 2 2",
